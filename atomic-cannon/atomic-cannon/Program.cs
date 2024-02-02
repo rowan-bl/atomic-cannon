84506af32521e340a4ffa1fcfad5261795515a5b
Rowan Blackbeard <rowan.blackbeard@yahoo.ca>
2024-02-02 09:07:48 -0700

    Comments, adding error handling, changed values to int

--- atomic-cannon/atomic-cannon/Program.cs
+++ atomic-cannon/atomic-cannon/Program.cs
@@ -1,5 +1,9 @@
-﻿double velocity;
-double angle;
+﻿using System.Transactions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//i chose to set velocity and angle to an int to make it easier to hit targets 
+int velocity;
+int angle;
 double userDist;
 double correctDist;
 double radians;
@@ -11,7 +15,7 @@ Console.WriteLine("  /__\\ (_  _)(  _  )(  \\/  )(_  _)/ __)   / __)  /__\\  ( \
 Console.WriteLine(" /(__)\\  )(   )(_)(  )    (  _)(_( (__   ( (__  /(__)\\  )  (  )  (  )(_)(  )  ( ");
 Console.WriteLine("(__)(__)(__) (_____)(_/\\/\\_)(____)\\___)   \\___)(__)(__)(_)\\_)(_)\\_)(_____)(_)\\_)");
 Console.WriteLine();
-Console.WriteLine();
+Console.WriteLine();    
 Console.WriteLine();
 Console.WriteLine("You will have 5 shots to locate and destroy the building.");
 Console.WriteLine("After each shot you will be told if your shot was farther or closer than the target.");
@@ -19,16 +23,29 @@ Console.WriteLine("Good luck! Press any key to start the game...");
 Console.ReadKey();
 
 Console.Clear();
-Console.Write("What power level would you like to use on your shot (1-100): ");
-velocity = Convert.ToInt32(Console.ReadLine());
+Console.Write("What velocity would you like to use on your shot (1-100): ");
+while (!int.TryParse(Console.ReadLine(), out velocity)|| velocity < 1 || velocity > 101 )
+{
+    Console.WriteLine("Enter a valid velocity between 1 and 100: ");
+}
 Console.WriteLine();
 Console.Write("What angle would you like to use on your shot (in degrees): ");
-angle = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
+while (!int.TryParse(Console.ReadLine(), out angle)|| angle < 0 || angle > 90)
+{
+    Console.WriteLine("Enter a valid angle between 1-89: ");
+}
 
+
+//converting degrees to radians for Math.Sin
 radians = angle * Math.PI / 180.0;
-userDist = (Math.Pow(velocity, 2)*Math.Sin(2*radians))/ 9.81;
-Console.WriteLine((int)Math.Round(userDist));
+
+
+//calculation 
+userDist = (Math.Pow(velocity, 2) * Math.Sin(2 * radians)) / 9.81;
+
+Console.WriteLine(Math.Round(userDist));
+
+
 
 
 
